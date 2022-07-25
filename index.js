a6dffeab53b35dee2adb1e03488f751ada326887
Felix Cagatin <cagatingilbert@gmail.com>
2022-07-25 10:13:08 -0700

    removed duplicate inquirer import

--- index.js
+++ index.js
@@ -7,8 +7,6 @@ const Employee = require('./lib/Employee');
 const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
-const inquirer = require('inquirer');
-
 
 // Function to prompt manager questions
 function getManagerInfo() {
