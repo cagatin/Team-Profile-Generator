a8df000ba45e5cffb6cdd4848b68f081cec3b395
Felix Cagatin <cagatingilbert@gmail.com>
2022-07-25 15:23:49 -0700

    Created function to generate Engineer card

--- src/helper.js
+++ src/helper.js
@@ -110,6 +110,12 @@ function generateTemplate() {
     `
 }
 
+
+//funciton to generate the header
+function generateHeader(name) {
+
+}
+
 // helper function to generate the Manager card
 function generateManagerCard(manager) {
     const name = manager.managerName;
@@ -141,3 +147,32 @@ function generateManagerCard(manager) {
 }
 
 // helper function to generate Engineer Card
+function generateEngineerCard(engineer) {
+    const name = engineer.engineerName;
+    const id = engineer.engineerID;
+    const email = engineer.engineerEmail;
+    const github = engineer.engineerGithub;
+
+    let engineerCard = `
+    <div class="card engineerCard" style="width: 20rem;">
+        <div class="card-header engineerHeader">
+            <h2 class="card-title">Engineer</h2>
+        </div>
+        <div class="card-body">
+            <ul class="list-group card-list">
+                <li class="list-group-item card-list-item"><span class="itemTitle">Name:</span> Felix Cagatin
+                </li>
+                <li class="list-group-item card-list-item"><span class="itemTitle">ID Number:</span> 02123
+                </li>
+                <li class="list-group-item card-list-item"><span class="itemTitle">Email:</span> <a
+                        href="mailto:EMAIL GOES HERE">Email@email.com</a>
+                </li>
+                <li class="list-group-item card-list-item"><span class="itemTitle">Github Account:</span> <a
+                        href="#">Github User</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+    `
+    return engineerCard;
+}
