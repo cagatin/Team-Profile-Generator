6c76029c457f5d13e45f7dc27c9a13b5daeb8524
Felix Cagatin <cagatingilbert@gmail.com>
2022-07-25 11:05:53 -0700

    Added bootstrap to html file

--- src/helper.js
+++ src/helper.js
@@ -3,20 +3,25 @@ function generateTemplate() {
     let htmlTemplate = `
     <!DOCTYPE html>
     <html lang="en">
-
+    
     <head>
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Team Profile</title>
+        <!-- Add Bootstrap -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
+    
         <link rel="stylesheet" href="styles.css">
     </head>
-
-    <body>
     
-
+    <body>
+        <header></header>
+    
+        <!-- Boostrap Script -->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
     </body>
-
+    
     </html>
     `
 }
