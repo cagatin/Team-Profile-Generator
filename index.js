cd7c06e781458cf4db06df4b9754f0a96df9901e
Felix Cagatin <cagatingilbert@gmail.com>
2022-07-25 10:12:31 -0700

    Created a function to prompt engineer questions

--- index.js
+++ index.js
@@ -40,5 +40,48 @@ function getManagerInfo() {
                     name: "managerOfficeNum"
                 }
             ]
-        );
+        )
+        // After the user finishes the prompts, create a new Manager Object
+        .then(answers => {
+            let manager = new Manager(answers.managerName, answers.managerID, answers.managerEmail, answers.managerOfficeNum);
+        })
+        .catch(err => console.log(err));
+}
+
+// Function to prompt Engineer Questions
+function getEngineerInfo() {
+    inquirer
+        .prompt(
+            [
+                // Name of Engineer
+                {
+                    type: "input",
+                    message: "What is the name of the Engineer?",
+                    name: "engineerName"
+                },
+                // Employee ID of the Engineer
+                {
+                    type: "input",
+                    message: "What is the employee ID of the Engineer?",
+                    name: "engineerID"
+                },
+                // Email address of the manager
+                {
+                    type: "input",
+                    message: "What is Engineer's Email?",
+                    name: "engineerEmail"
+                },
+                // Github profile of the engineer
+                {
+                    type: "input",
+                    message: "What is the Engineer's GITHUB username?",
+                    name: "engineerGithub"
+                }
+            ]
+        )
+        // After the user finishes the prompts, create a new Engineer Object
+        .then(answers => {
+            let engineer = new Engineer(answers.engineerName, answers.engineerID, answers.engineerEmail, answers.engineerGithub);
+        })
+        .catch(err => console.log(err));
 }
