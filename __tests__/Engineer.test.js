b339c1c04933b0714663311af3d235f0b868189f
Felix Cagatin <cagatingilbert@gmail.com>
2022-07-27 13:54:04 -0700

    Added test:jest property

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,40 +1,36 @@
 const Engineer = require('../lib/Engineer');
 
 describe('Instantiation', () => {
+    const testEmployee = new Engineer('Felix', '12', 'cagatin');
     test("Name of Engineer", () => {
-        const testEmployee = new Employee('Felix', '12', 'Cagatingilbert@mail.com');
         const name = testEmployee.name;
         expect(name).toBe('Felix');
     })
     test("Engineer ID", () => {
-        const testEmployee = new Employee('Felix', '12', 'Cagatingilbert@mail.com');
         const id = testEmployee.id;
         expect(id).toBe('12');
     })
     test("Engineer email", () => {
-        const testEmployee = new Employee('Felix', '12', 'Cagatingilbert@mail.com');
         const email = testEmployee.email;
         expect(email).toBe('Cagatingilbert@gmail.com');
     })
-})
+});
+
 describe('Methods', () => {
+    const testEmployee = new Engineer('Felix', '12', 'cagatin');
     test("getName Function", () => {
-        const testEmployee = new Employee('Felix', '12', 'Cagatingilbert@mail.com');
         const name = testEmployee.getName();
         expect(name).toBe('Felix');
     })
     test("getId Function", () => {
-        const testEmployee = new Employee('Felix', '12', 'Cagatingilbert@mail.com');
         const id = testEmployee.getId();
         expect(id).toBe('12');
     })
     test("getEmail", () => {
-        const testEmployee = new Employee('Felix', '12', 'Cagatingilbert@mail.com');
         const email = testEmployee.getEmail();
         expect(email).toBe('Cagatingilbert@gmail.com');
     });
     test("getRole", () => {
-        const testEmployee = new Employee('Felix', '12', 'Cagatingilbert@mail.com');
         const role = testEmployee.getRole();
         expect(role).toBe('Employee');
     })
