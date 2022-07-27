8883b4f155fde00b256fb4997e3ffb4718111212
Felix Cagatin <cagatingilbert@gmail.com>
2022-07-26 18:02:11 -0700

    Added tests for methods

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -17,3 +17,20 @@ describe('Instantiation', () => {
         expect(email).toBe('Cagatingilbert@gmail.com');
     })
 })
+describe('Methods', () => {
+    test("getName Function", () => {
+        const testEmployee = new Employee('Felix', '12', 'Cagatingilbert@mail.com');
+        const name = testEmployee.getName();
+        expect(name).toBe('Felix');
+    })
+    test("getId Function", () => {
+        const testEmployee = new Employee('Felix', '12', 'Cagatingilbert@mail.com');
+        const id = testEmployee.getId();
+        expect(id).toBe('12');
+    })
+    test("Employee email", () => {
+        const testEmployee = new Employee('Felix', '12', 'Cagatingilbert@mail.com');
+        const email = testEmployee.getEmail();
+        expect(email).toBe('Cagatingilbert@gmail.com');
+    })
+})
