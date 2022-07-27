8394ef019fde706e69fd7c8d7b5a1c76b06897bf
Felix Cagatin <cagatingilbert@gmail.com>
2022-07-27 14:05:17 -0700

    Created test for Engineer class

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,41 +1,46 @@
-// const Engineer = require('../lib/Engineer');
+const Engineer = require('../lib/Engineer');
 
-// describe('Instantiation', () => {
-//     test("Name of Engineer", () => {
-//         const testEmployee = new Engineer('Felix', '12', 'cagatin');
-//         const name = testEmployee.name;
-//         expect(name).toBe('Felix');
-//     })
-//     test("Engineer ID", () => {
-//         const testEmployee = new Engineer('Felix', '12', 'cagatin');
-//         const id = testEmployee.id;
-//         expect(id).toBe('12');
-//     })
-//     test("Engineer email", () => {
-//         const testEmployee = new Engineer('Felix', '12', 'cagatin');
-//         const email = testEmployee.email;
-//         expect(email).toBe('Cagatingilbert@gmail.com');
-//     })
-// })
-// describe('Methods', () => {
-//     test("getName Function", () => {
-//         const testEmployee = new Engineer('Felix', '12', 'cagatin');
-//         const name = testEmployee.getName();
-//         expect(name).toBe('Felix');
-//     })
-//     test("getId Function", () => {
-//         const testEmployee = new Employee('Felix', '12', 'Cagatingilbert@mail.com');
-//         const id = testEmployee.getId();
-//         expect(id).toBe('12');
-//     })
-//     test("getEmail", () => {
-//         const testEmployee = new Employee('Felix', '12', 'Cagatingilbert@mail.com');
-//         const email = testEmployee.getEmail();
-//         expect(email).toBe('Cagatingilbert@gmail.com');
-//     });
-//     test("getRole", () => {
-//         const testEmployee = new Employee('Felix', '12', 'Cagatingilbert@mail.com');
-//         const role = testEmployee.getRole();
-//         expect(role).toBe('Employee');
-//     })
-// })
+describe('Instantiation', () => {
+    test("Name of Engineer", () => {
+        const testEngineer = new Engineer('Felix', '12', 'cagatingilbert@gmail.com', 'cagatin');
+        const name = testEngineer.name;
+        expect(name).toBe('Felix');
+    })
+    test("Engineer's ID", () => {
+        const testEngineer = new Engineer('Felix', '12', 'cagatingilbert@gmail.com', 'cagatin');
+        const id = testEngineer.id;
+        expect(id).toBe('12');
+    })
+    test("Engineer email", () => {
+        const testEngineer = new Engineer('Felix', '12', 'cagatingilbert@gmail.com', 'cagatin');
+        const email = testEngineer.email;
+        expect(email).toBe('cagatingilbert@gmail.com');
+    })
+})
+describe('Methods', () => {
+    test("getName method", () => {
+        const testEngineer = new Engineer('Felix', '12', 'cagatingilbert@gmail.com', 'cagatin');
+        const name = testEngineer.getName();
+        expect(name).toBe('Felix');
+    })
+    test("getId method", () => {
+        const testEngineer = new Engineer('Felix', '12', 'cagatingilbert@gmail.com', 'cagatin');
+        const id = testEngineer.getId();
+        expect(id).toBe('12');
+    })
+    test("getEmail method", () => {
+        const testEngineer = new Engineer('Felix', '12', 'cagatingilbert@gmail.com', 'cagatin');
+        const email = testEngineer.getEmail();
+        expect(email).toBe('cagatingilbert@gmail.com');
+    });
+    test("getGithub method", () => {
+        const testEngineer = new Engineer('Felix', '12', 'cagatingilbert@gmail.com', 'cagatin');
+        const github = testEngineer.getGithub();
+        expect(github).toBe('cagatin');
+    })
+    test("getRole", () => {
+        const testEngineer = new Engineer('Felix', '12', 'cagatingilbert@gmail.com', 'cagatin');
+        const role = testEngineer.getRole();
+        expect(role).toBe('Engineer');
+    });
+})
